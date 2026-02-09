816d7bc63dfdc4432bd20cfad96265483ec560dc
caioamorimcel <caio.amorim.cel@gmail.com>
2026-02-09 09:48:41 -0300

    lala

---
+++ src/routes/leitura/+page.svelte
@@ -0,0 +1,23 @@
+
+
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  let linhas: string[] = [];
+
+  onMount(async () => {
+    const res = await fetch('/leituraTeste.txt');
+    const texto = await res.text();
+
+    linhas = texto.split(/\r?\n/).filter(l => l.trim() !== '');
+
+  });
+</script>
+
+<h2>Linhas do arquivo:</h2>
+
+<ul>
+  {#each linhas as linha}
+    <li>{linha}</li>
+  {/each}
+</ul> 
