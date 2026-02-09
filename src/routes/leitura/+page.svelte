39edfe838cd4089540b2f10418536ad070c649d4
caioamorimcel <caio.amorim.cel@gmail.com>
2026-02-09 11:27:05 -0300

    2-commit

--- src/routes/leitura/+page.svelte
+++ src/routes/leitura/+page.svelte
@@ -1,23 +1,31 @@
-
-
 <script lang="ts">
   import { onMount } from 'svelte';
 
-  let linhas: string[] = [];
+  type Item = {
+    x1: number;
+    y1: number;
+    x2: number;
+    y2: number;
+    texto: string;
+  };
+
+  let itens: Item[] = [];
 
   onMount(async () => {
-    const res = await fetch('/leituraTeste.txt');
-    const texto = await res.text();
-
-    linhas = texto.split(/\r?\n/).filter(l => l.trim() !== '');
-
+    const res = await fetch('/Liga_da_Justiça_Odisseia_01/DC_16_(28).json');
+    itens = await res.json(); 
   });
 </script>
 
-<h2>Linhas do arquivo:</h2>
+<h2>Itens do arquivo JSON:</h2>
 
 <ul>
-  {#each linhas as linha}
-    <li>{linha}</li>
+  {#each itens as item}
+    <li>
+      x1: {item.x1}, y1: {item.y1}, x2: {item.x2}, y2: {item.y2} <br>
+      texto: {item.texto}
+    </li>
   {/each}
-</ul> 
+</ul>
+
+
